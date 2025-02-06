9872f718a053416bf075f7c4f6da4942f53742b9
suswanadalahaku <suswanadalahaku@gmail.com>
2025-02-06 21:02:47 +0700

    Update 06/02/2025-21:02 favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel=”icon” href="{{ asset('assets/logo.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('assets/healthbar-logo1.png') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
